15c1eb782720f200611caac92dfd5a13b46b0ce1
pcwii <pmchenier@gmail.com>
2018-07-23 15:20:54 -0400

    first draft

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from os.path import dirname
-import time
 
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler, intent_file_handler
@@ -19,12 +18,19 @@ class MuteSkill(MycroftSkill):
     def __init__(self):
         super(MuteSkill, self).__init__(name="MuteSkill")
         self.mute = False
-        self.current_volume
+        self.current_volume = 0
 
     def initialize(self):
         self.load_data_files(dirname(__file__))
 
-        #  Check and then monitor for credential changes
+    @intent_handler(IntentBuilder('MuteIntent').require("MuteKeyword").build())
+    def handle_mute_intent(self, message):
+        self.current_volume = self.mixer.getvolume()[0]
+        self.mixer.setvolume(0)
+        self.mute = True
 
-    @intent_handler(IntentBuilder('BeSilentIntent').require("MuteKeyword").build())
-    def handle_be_silent_intent(self, message):
+    @intent_handler(IntentBuilder('UnMuteIntent').require("UnMuteKeyword").build())
+    def handle_un_mute_intent(self, message):
+        if self.mute:
+            self.mixer.setvolume(self.current_volume)
+            self.mute = False
