fabc1413200197b57caa15c5328686d683f54c63
pcwii <pmchenier@gmail.com>
2018-07-23 16:09:27 -0400

    first draft

--- __init__.py
+++ __init__.py
@@ -19,23 +19,30 @@ class MuteSkill(MycroftSkill):
     """
     def __init__(self):
         super(MuteSkill, self).__init__(name="MuteSkill")
-        self.mute = False
+        self.mute_state = False
         self.current_volume = 0
+        self.mute_enable = True
 
     def initialize(self):
         self.load_data_files(dirname(__file__))
+        try:
+            self.mixer = Mixer()
+        except:
+            self.mute_enable = False
 
     @intent_handler(IntentBuilder('MuteIntent').require("MuteKeyword").build())
     def handle_mute_intent(self, message):
-        self.current_volume = self.mixer.getvolume()[0]
-        self.mixer.setvolume(0)
-        self.mute = True
+        if self.mute_enable:
+            self.current_volume = self.mixer.getvolume()[0]
+            self.mixer.setvolume(0)
+            self.mute_state = True
 
     @intent_handler(IntentBuilder('UnMuteIntent').require("UnMuteKeyword").build())
     def handle_un_mute_intent(self, message):
-        if self.mute:
-            self.mixer.setvolume(self.current_volume)
-            self.mute = False
+        if self.mute_enable:
+            if self.mute_state:
+                self.mixer.setvolume(self.current_volume)
+                self.mute_state = False
 
     def stop(self):
         pass
